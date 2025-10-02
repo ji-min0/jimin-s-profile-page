394df5f7fd7631b3807fc0b0466c4e32626a92c8
ji-min0 <k.jimin2002@gmail.com>
2025-10-02 10:40:25 +0900

    feat: add iCloud SMTP support

--- app/core/config.py
+++ app/core/config.py
@@ -21,14 +21,17 @@ class Settings(BaseSettings):
         super().__init__(**values)
         if self.EMAIL_PROVIDER == "naver":
             self.EMAIL_HOST = "smtp.naver.com"
-            self.EMAIL_PORT = 465 # 여기 수정함
+            self.EMAIL_PORT = 465
+        elif self.EMAIL_PROVIDER == "icloud":
+            self.EMAIL_HOST = "smtp.mail.me.com"
+            self.EMAIL_PORT = 587
         elif self.EMAIL_PROVIDER == "gmail":
             self.EMAIL_HOST = "smtp.gmail.com"
             self.EMAIL_PORT = 587
             self.USE_SSL = False
         else:
             raise ValueError(
-                "지원하지 않는 메일 서비스입니다. (naver/gmail만 사용 가능)"
+                "지원하지 않는 메일 서비스입니다. (naver/gmail/icloud만 사용 가능)"
             )
 
 
